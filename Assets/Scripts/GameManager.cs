728195e195625486f14d73e03e25055d39e96e0d
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2021-10-02 12:50:59 -0400

    Add basic Game Logic
    - Add GameManager
    - Add operator overload for Pillar sum calculations

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState { InProgress, Loss, Victory }
+
+    public GameState CurrentGameState = GameState.InProgress;
+    public int CurrentTurn { get; set; } = 0;
+    public Pillars Pillars { get; set; } = new();
+
+    [SerializeField] private int winTurn = 25;
+    [SerializeField] private int maximumPillar = 100;
+    [SerializeField] private int minimumPillar = 0;
+
+    private MapManager _mapManager;
+
+    private void Awake()
+    {
+        _mapManager = FindObjectOfType<MapManager>();
+    }
+
+    private void ProcessTurn()
+    {
+        UpdatePillarValues();
+        CheckLoseConditions();
+        CheckWinConditions();
+
+        CurrentTurn++;
+    }
+
+    private void UpdatePillarValues()
+    {
+        Pillars deltaPillars = new Pillars();
+
+        foreach (Cell cell in _mapManager.Cells)
+        {
+            if (cell.Tile != null)
+            {
+                deltaPillars += cell.Tile.Pillars;
+            }
+        }
+
+        Pillars += deltaPillars;
+    }
+
+    private void CheckLoseConditions()
+    {
+        if (Pillars.Military <= minimumPillar)
+        {
+            Debug.Log("Your military was too weak and you were overthrown by the people.");
+            CurrentGameState = GameState.Loss;
+        }
+
+        if (Pillars.Military >= maximumPillar)
+        {
+            Debug.Log("Your military was too strong and you were overthrown in a military coup.");
+            CurrentGameState = GameState.Loss;
+        }
+
+        if (Pillars.Economy <= minimumPillar)
+        {  
+            Debug.Log("Your failed to maintain a minimum economic supply and your population starved.");
+            CurrentGameState = GameState.Loss;
+        }
+
+        if (Pillars.Economy >= maximumPillar)
+        {
+            Debug.Log("Your planned economy collapsed due to an overabundance of supply.");
+            CurrentGameState = GameState.Loss;
+        }
+
+        if (Pillars.Culture <= minimumPillar)
+        {
+            Debug.Log("Your influence over your population dwindled and your state slowly dissolved.");
+            CurrentGameState = GameState.Loss;
+        }
+
+        if (Pillars.Culture >= maximumPillar)
+        {
+            Debug.Log("Your grip on the population became too tight and rebel groups staged a coup. Long live the resistance!");
+            CurrentGameState = GameState.Loss;
+        }
+    }
+
+    private void CheckWinConditions()
+    {
+        if (CurrentTurn >= winTurn && CurrentGameState == GameState.InProgress)
+        {
+            CurrentGameState = GameState.Victory;
+        }
+    }
+}
